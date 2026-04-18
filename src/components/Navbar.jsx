cd303ca2c2338610e8efd4d83cfe8d5bffe0c7e2
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-18 15:52:28 +0500

    coding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,16 @@ import '../App.css'
 function Navbar() {
     return (
         <div className="main">
-           <h2>ABDUL REHMAN</h2> 
-          <nav >
-            <ul className='navlinks'>
-                <li><a href=""> <i class="fa-solid fa-house"></i>Home</a></li>
-                <li><a href=""><i class="fa-solid fa-circle-user"></i>About</a></li>
-                <li><a href=""><i class="fa-solid fa-briefcase"></i>My Work</a></li>
-                <li><a href=""><i class="fa-jelly-fill fa-regular fa-address-card"></i>Contact</a></li>
-            </ul>
-          </nav>
-            <button className='signupbtn'><i class="fa-solid fa-arrow-right-to-bracket"></i> Sign Up</button>
+            <h2>ABDUL REHMAN</h2>
+            <nav >
+                <ul className='navlinks'>
+                    <li><a href=""> <i class="fa-solid fa-house"></i>Home</a></li>
+                    <li><a href=""><i class="fa-solid fa-circle-user"></i>About</a></li>
+                    <li><a href=""><i class="fa-solid fa-briefcase"></i>My Work</a></li>
+                    <li><a href=""><i class="fa-jelly-fill fa-regular fa-address-card"></i>Contact</a></li>
+                </ul>
+            </nav>
+            <button className='signupbtn'><a href=""><i class="fa-solid fa-arrow-right-to-bracket"></i> Sign Up</a></button>
         </div>
     )
 }
