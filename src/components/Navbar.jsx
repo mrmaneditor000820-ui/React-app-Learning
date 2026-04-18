1e4065941406fdad1ea90252b89cc7fc570fcbc3
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-18 08:28:20 +0500

    icons added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 
 import '../App.css'
 
+
 function Navbar() {
     return (
         <div className="main">
            <h2>ABDUL REHMAN</h2> 
           <nav >
             <ul className='navlinks'>
-                <li>Home</li>
-                <li>About</li>
-                <li>My CV</li>
-                <li>Contact</li>
+                <li><a href=""> <i class="fa-solid fa-house"></i>Home</a></li>
+                <li><a href=""><i class="fa-solid fa-circle-user"></i>About</a></li>
+                <li><a href=""><i class="fa-solid fa-briefcase"></i>My Work</a></li>
+                <li><a href=""><i class="fa-jelly-fill fa-regular fa-address-card"></i>Contact</a></li>
             </ul>
           </nav>
-            <button>Sign Up</button>
+            <button className='signupbtn'><i class="fa-solid fa-arrow-right-to-bracket"></i> Sign Up</button>
         </div>
     )
 }
