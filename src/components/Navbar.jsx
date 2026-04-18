be81f7775c02f84758059a761765607d03f0dd6b
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-18 07:49:15 +0500

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ function Navbar() {
     return (
         <div className="main">
            <h2>ABDUL REHMAN</h2> 
-          <nav>
-            <ul>
+          <nav >
+            <ul className='navlinks'>
                 <li>Home</li>
                 <li>About</li>
                 <li>My CV</li>
                 <li>Contact</li>
-                
             </ul>
           </nav>
+            <button>Sign Up</button>
         </div>
     )
 }
