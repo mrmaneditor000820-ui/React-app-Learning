200df999c9512d5cb8e1bfa23459e58f36a0f486
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-22 20:08:33 +0500

    products data created

--- src/components/utils/Product.jsx
+++ src/components/utils/Product.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
 
-function Product() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default Product
+const products = [{
+    title: "SHOES1",
+    Price: "200",
+    Discription: "This is my shoes",
+    img: <img src="" alt="" />
+},
+{
+    title: "SHOES2",
+    Price: "200",
+    Discription: "This is my shoes",
+    img: <img src="" alt="" />
+},
+{
+    title: "SHOES3",
+    Price: "200",
+    Discription: "This is my shoes",
+    img: <img src="" alt="" />
+}]
