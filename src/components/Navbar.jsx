d3a74b194a3fb10af1e24a9e8a06439a58e283d5
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-23 22:26:56 +0500

    coding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ function Navbar() {
             <h2>ABDUL REHMAN</h2>
             <nav >
                 <ul className='navlinks'>
-                    <li><a href=""> <i class="fa-solid fa-house"></i>Home</a></li>
-                    <li><a href=""><i class="fa-solid fa-circle-user"></i>About</a></li>
-                    <li><a href=""><i class="fa-solid fa-briefcase"></i>My Work</a></li>
-                    <li><a href=""><i class="fa-jelly-fill fa-regular fa-address-card"></i>Contact</a></li>
+                    <li><a href=""><span id='kuchbhi'><i class="fa-solid fa-house"></i></span><br></br>Home</a></li>
+                    <li><a href=""><span><i class="fa-solid fa-circle-user"></i></span><br></br>About</a></li>
+                    <li><a href=""><span><i class="fa-solid fa-briefcase"></i></span><br></br>My Work</a></li>
+                    <li><a href=""><span><i class="fa-jelly-fill fa-regular fa-address-card"></i></span><br></br>Contact</a></li>
                 </ul>
             </nav>
             <button className='signupbtn'><a href=""><i class="fa-solid fa-arrow-right-to-bracket"></i> Sign Up</a></button>
