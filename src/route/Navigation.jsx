50d6367a3e40e2e6e813a56d251a6bcba8dce29f
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-26 20:15:30 +0500

    router navbar added

--- src/route/Navigation.jsx
+++ src/route/Navigation.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 import  {BrowserRouter,Route, Routes} from 'react-router-dom'
 import Home from "../pages/Home"
 import About from "../pages/About"
+import Navbar from '../components/Navbar'
 
 function Navigation() {
     return (
       <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/About' element={<About/>}/>
