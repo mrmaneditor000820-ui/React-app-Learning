bba2d0a162bb2d8b42cb2b7a28fb652624da02c0
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-26 20:17:38 +0500

    pages added to main navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom'
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <Link to={"/about"}>ABOUT</Link>
+    
         </div>
     )
 }
